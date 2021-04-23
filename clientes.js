1f8491a2f4b65cd1ce771892838c4b4f3786e413
starcomunicaciones <82775040+starcomunicaciones@users.noreply.github.com>
2021-04-23 14:29:16 -0500

    Add files via upload

---
+++ clientes.js
@@ -0,0 +1,63 @@
+var cedula = prompt("Por favor ingresa tu numero de cedula para buscar tu usuario");
+
+
+if (cedula == 1083881513)
+	{location.href='inicio.html'}
+
+else if (cedula == 1083891603) {
+	location.href='01.html'
+}
+else if (cedula == 2) {
+	location.href='02.html'
+}
+else if (cedula == 3) {
+	location.href='03.html'
+}
+else if (cedula == 4) {
+	location.href='04.html'
+}
+
+else if (cedula == 5) {
+	location.href='05.html'
+}
+else if (cedula == 6) {
+	location.href='06.html'
+}
+else if (cedula == 7) {
+	location.href='07.html'
+}
+else if (cedula == 8) {
+	location.href='08.html'
+}
+else if (cedula == 9) {
+	location.href='09.html'
+}
+else if (cedula == 10) {
+	location.href='10.html'
+}
+else if (cedula == 11) {
+	location.href='11.html'
+}
+else if (cedula == 12) {
+	location.href='12.html'
+}
+else if (cedula == 13) {
+	location.href='13.html'
+}
+else if (cedula == 14) {
+	location.href='14.html'
+}
+else if (cedula == 15) {
+	location.href='15.html'
+}
+else if (cedula == 16) {
+	location.href='16.html'
+}
+else if (cedula == 17) {
+	location.href='17.html'
+}
+
+
+else {
+	document.write("usuario no encontardo");
+}
